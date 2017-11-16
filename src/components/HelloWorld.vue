271268ff2464aad93e68958c96cd5dedbc6c7f02
yaochen <1219716595@qq.com>
2017-11-16 09:55:50 +0800

    简单的项目分支

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <section>
+    test
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
